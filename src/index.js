b28801a0935f5fee414df9fcdc56e49cc9bec3c2
DESKTOP-N55TG2P\dmk <fathur@dmk.co.id>
2021-11-03 14:44:39 +0700

    Import Pages Home

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Home from './Pages/Home/Home'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
