48b6bc7bb712a801f7a16bc97566e507626c1d79
fathur <fathurpaoel41@gmail.com>
2021-11-03 14:46:31 +0700

    test edit

--- src/Components/FooterBar.js
+++ src/Components/FooterBar.js
@@ -10,7 +10,7 @@ export default function FooterBar() {
                         class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
                     >
                         <svg class="bi" width="30" height="24">
-                            {/* <use xlink:href="#bootstrap" /> */}
+                            {/* <use xlink:href="#bootstrap" /> */} 
                         </svg>
                     </a>
                     <span class="text-muted">
