8c7d48c50fab95e9bd11b9d6a99c6ca481f57b7e
DESKTOP-N55TG2P\dmk <fathur@dmk.co.id>
2021-11-03 10:21:35 +0700

    Test Button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button>sdfds</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
