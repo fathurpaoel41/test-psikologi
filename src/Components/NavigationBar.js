b0d9e7213fe003a80b56f1be2c90ea765544380b
fathur <fathurpaoel41@gmail.com>
2021-11-14 11:30:05 +0700

    Navigation Bar fix the route

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -1,7 +1,20 @@
 import React from "react"
 import { Container, Navbar, Nav } from "react-bootstrap"
+import { Routes, Route, Link } from "react-router-dom"
+import Contact from "../Pages/Contact/Contact"
+import Home from "../Pages/Home/Home"
+import About from "../Pages/About/About"
+import "../Assets/Style.css"
+import DesclaimerSoal from "../Pages/Declaimer/DesclaimerSoal"
 
 export default function NavigationBar() {
+
+        const styleLink = {
+            textDecoration: "none",
+            color: "white"
+        }
+
+
     return (
         <div>
             <Navbar expand="lg" bg="dark" variant="dark">
@@ -10,13 +23,33 @@ export default function NavigationBar() {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">About</Nav.Link>
-                            <Nav.Link href="#link">Contact</Nav.Link>
+                            <Nav.Link>
+                                <Link to="/" style={styleLink}>Home</Link>
+                            </Nav.Link>
+                            <Nav.Link>
+                                <Link to="/about" style={styleLink}>About</Link>
+                            </Nav.Link>
+                            <Nav.Link>
+                                <Link to="/contact" style={styleLink}>Contact</Link>
+                            </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/soal" element={<DesclaimerSoal />} />
+                <Route
+                    path="*"
+                    element={
+                        <main style={{ padding: "1rem" }}>
+                            <p>There's nothing here!</p>
+                        </main>
+                    }
+                />
+            </Routes>
         </div>
     )
 }
