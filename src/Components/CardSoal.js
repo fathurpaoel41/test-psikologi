b5f4d145ec9eb894ab6c78698acd966be78edf4a
fathur <fathurpaoel41@gmail.com>
2021-11-22 08:27:36 +0700

    Fixing Component CardSoal

--- src/Components/CardSoal.js
+++ src/Components/CardSoal.js
@@ -1,20 +1,26 @@
 import React from "react"
-import { Card, Button, Col } from "react-bootstrap"
+import { Card, Col } from "react-bootstrap"
+import { Link } from "react-router-dom"
 
-export default function CardSoal() {
+export default function CardSoal(props) {
+    const styleLink = {
+        textDecoration: "none",
+        color: "black",
+    }
+
     return (
         <Col md={4} style={{ padding: "10px", color: "black" }}>
-            <Card style={{ width: "18rem" }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and
-                        make up the bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-            </Card>
+            <Link to={`/soal/${props.id}`} style={styleLink}>
+                <Card style={{ width: "18rem" }}>
+                    <Card.Img variant="top" src={props.img} />
+                    <Card.Body>
+                        <Card.Title>{props.title}</Card.Title>
+                        <Card.Text>
+                            {props.description}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Link>
         </Col>
     )
 }
