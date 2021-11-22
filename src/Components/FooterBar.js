f4c04f2329798f83ac20d74534cfce9ce2604919
fathur <fathurpaoel41@gmail.com>
2021-11-22 08:28:34 +0700

    Fixing className in FooterBar.js

--- src/Components/FooterBar.js
+++ src/Components/FooterBar.js
@@ -3,34 +3,34 @@ import React from "react"
 export default function FooterBar() {
     return (
         <div style={{bottom: "0",position: "relative"}}>
-            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                <div class="col-md-4 d-flex align-items-center">
+            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                <div className="col-md-4 d-flex align-items-center">
                     <a
                         href="/"
-                        class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
+                        className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
                     >
-                        <svg class="bi" width="30" height="24">
+                        <svg className="bi" width="30" height="24">
                             {/* <use xlink:href="#bootstrap" /> */} 
                         </svg>
                     </a>
-                    <span class="text-muted">
+                    <span className="text-muted">
                         <span style={{ color: "white" }}>
                             {/* tambahin logo dimarih */}
                             &copy; 2021 Muhammad Fathurachman
                         </span>
                     </span>
                 </div>
-                <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-                    <li class="ms-3">
-                        <i class="fa fa-twitter" aria-hidden="true"></i>
+                <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+                    <li className="ms-3">
+                        <i className="fa fa-twitter" aria-hidden="true"></i>
                     </li>
-                    <li class="ms-3">
-                        <i class="fa fa-instagram" aria-hidden="true"></i>
+                    <li className="ms-3">
+                        <i className="fa fa-instagram" aria-hidden="true"></i>
                     </li>
-                    <li class="ms-3">
-                        <i class="fa fa-facebook" aria-hidden="true"></i>
+                    <li className="ms-3">
+                        <i className="fa fa-facebook" aria-hidden="true"></i>
                     </li>
-                    <li class="ms-3">&nbsp;</li>
+                    <li className="ms-3">&nbsp;</li>
                 </ul>
             </footer>
             &nbsp;
