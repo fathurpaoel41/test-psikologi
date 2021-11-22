f0ea7b17b6a42cfcced577ed15b3f40c6be1518e
fathur <fathurpaoel41@gmail.com>
2021-11-22 08:32:02 +0700

    Create Page HasilTest.js

---
+++ src/Pages/Hasil/HasilTest.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react"
+import Content from "../../Components/Content"
+import { Card, Col, Row } from "react-bootstrap"
+import CardSoal from "../../Components/CardSoal"
+
+export default class HasilTest extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <Content>
+                <Col md={12} style={{ color: "black" }}>
+                    <Card>
+                        <Card.Img variant="top" />
+                        <Card.Body>
+                            <Card.Title>Hasil Test Anda</Card.Title>
+                            <Card.Text>
+                                <h2>
+                                    <center>
+                                        <b>Skor Anda : </b>
+                                    </center>
+                                </h2>
+                                <br />
+                                hasil yag kamu dapatkan bla blablablabla
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <div className="py-4">
+                    <Row className="show-grid">
+                        <Col lg={12}>
+                            <h1>
+                                <center>
+                                    Daftar Soal yang bisa kamu coba !
+                                </center>
+                            </h1>
+                            <Row className="show-grid d-flex justify-content-center">
+                            </Row>
+                        </Col>
+                    </Row>
+                </div>
+            </Content>
+        )
+    }
+}
