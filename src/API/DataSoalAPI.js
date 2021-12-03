47e0508c8413b1bd10a698ef7643e4e819f778cc
fathur <fathurpaoel41@gmail.com>
2021-12-04 06:38:14 +0700

    Add function getDataSoal for consume API get Data Soal

--- src/API/DataSoalAPI.js
+++ src/API/DataSoalAPI.js
@@ -2,7 +2,7 @@ import BaseUrl from "./BaseUrl"
 import axios from "axios"
 
 export default class DataSoalAPI extends BaseUrl {
-    subPath = "dataSoal"
+    subPath = "postTest"
 
     getDataSoal() {
         return axios.get(this.endpoint(this.subPath))
