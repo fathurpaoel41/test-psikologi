4dd1a852918021e0806242cdd793d4c0c5238153
fathur <fathurpaoel41@gmail.com>
2021-12-04 06:39:44 +0700

    Setting and store point in CardTestMultipleChoice

--- src/Components/CardTestMultipleChoice.js
+++ src/Components/CardTestMultipleChoice.js
@@ -1,42 +1,74 @@
+import React, { Component } from 'react'
 import { Test, QuestionGroup, Question, Option } from "react-multiple-choice"
 import { Card } from "react-bootstrap"
-import React from "react"
+export default class CardTestMultipleChoice extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { 
+            optionTest : props.pilihan,
+        }
+        this.StyleOPtions = {
+            option: {
+                width : "100%",
+            }
+        }
 
-export default function CardTestMultipleChoice() {
-
-    const StyleOptions = {
-        option: {
-            width: "100%"
-          }
+        this.savePoint = this.savePoint.bind(this)
     }
 
-    return (
-        <>
+    savePoint(point){
+        let firstKey = Object.keys(point)[0]
+        let firstValue = Object.values(point)[0]
+        let objectPoint = {
+            nomor :firstKey,
+            point : firstValue,
+        }
+     
+        this.props.savePointCallback(objectPoint)
+    }
+
+    render() {
+        return (
+            <>
             <div className="d-flex justify-content-center">
                 <Card className="StyleCard">
                     <Card.Body>
-                        <Test onOptionSelect={(selectedOptions) => console.log(selectedOptions)} style={{width: '100%'}}>
-                            <QuestionGroup questionNumber={0}>
+                        <Test onOptionSelect={(selectedOptions) =>this.savePoint(selectedOptions)} style={{ width: "100%" }}>
+                            <QuestionGroup questionNumber={this.state.optionTest.nomor}>
                                 <Card.Text>
                                     <Question className="StyleQuestion">
-                                        <p align="justify" style={{width: '100%',color: 'black'}}>
-                                            pada suatu hari, andi sedang berjalan
-                                            pada sebuah kapal yang sangat besar.
-                                            sehingga tatapannya sangat luas. dan dia menemukan seseorang yang amat cantik
+                                        <p
+                                            align="justify"
+                                            style={{
+                                                width: "100%",
+                                                color: "black",
+                                            }}
+                                        >
+                                            {this.state.optionTest.pertanyaan}
                                         </p>
                                     </Question>
                                 </Card.Text>
-                                <Option value="1" style={StyleOptions}>
-                                    Steak
-                                </Option>
-                                <Option value="2" style={StyleOptions}>Sushi</Option>
-                                <Option value="3" style={StyleOptions}>Pad Thai</Option>
+                                {this.state.optionTest.pilihan.map(function (res) {
+                                    return (
+                                        <Option
+                                            value={res.point}
+                                            style={{
+                                                optionTest : {
+                                                    width : '100%'
+                                                }
+                                            }}
+                                        >
+                                            {res.soal}
+                                        </Option>
+                                    )
+                                })}
                             </QuestionGroup>
                         </Test>
                     </Card.Body>
                 </Card>
             </div>
-        <br />
+            <br />
         </>
-    )
+        )
+    }
 }
