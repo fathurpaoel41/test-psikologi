58427fd6d06adcaa6fbb0ae02e0e2b16847fd812
fathur <fathur@dmk.co.id>
2021-12-23 16:19:17 +0700

    Revert "Test Revert"
    This reverts commit 3c32f1d359071dbdd53c8fd7ba7b0ebbf3af0fe1.

--- src/Components/CardTestMultipleChoice.js
+++ src/Components/CardTestMultipleChoice.js
@@ -24,7 +24,6 @@ export default class CardTestMultipleChoice extends Component {
             point : firstValue,
         }
      
-        
         this.props.savePointCallback(objectPoint)
     }
 
