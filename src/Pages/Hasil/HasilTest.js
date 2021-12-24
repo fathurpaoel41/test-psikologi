a7b31a86b4570c5578b9d71854876c2f9ab174ec
fathur <fathur@dmk.co.id>
2021-12-24 07:50:53 +0700

    Revert "Add some another question for user"
    This reverts commit 538c90dc14bbe6daaba20450c4bb7b0a897c6f16.

--- src/Pages/Hasil/HasilTest.js
+++ src/Pages/Hasil/HasilTest.js
@@ -1,35 +1,18 @@
 import React, { Component } from "react"
 import Content from "../../Components/Content"
-import { Card, Col, Row,Spinner } from "react-bootstrap"
+import { Card, Col, Row } from "react-bootstrap"
 import CardSoal from "../../Components/CardSoal"
-import DataSoalAPI from "../../API/DataSoalAPI"
-import loadable from '@loadable/component'
-
-const CardSoalLoad = loadable(() => import("../../Components/CardSoal"), {
-    fallback: <div><center><Spinner animation="border" variant="primary" /></center></div>,
-  })
 
 export default class HasilTest extends Component {
     constructor(props) {
         super(props)
-        this.dataSoalAPI = new DataSoalAPI()
         
         const getTotalPoint = localStorage.getItem("totalPoint")
 
         this.state = {
-            dataSoal : [],
             total_points: getTotalPoint,
         }
     }
-
-    async componentDidMount() {
-        let api = await this.dataSoalAPI.getDataSoal().then((data) => {
-            this.setState({dataSoal : data.data})
-        }).catch((error) => {
-            console.log("error = ",error)
-        })
-    }
-
     render() {
         return (
             <Content>
@@ -53,18 +36,12 @@ export default class HasilTest extends Component {
                 <div className="py-4">
                     <Row className="show-grid">
                         <Col lg={12}>
-                            <h1><center>Daftar Soal yang bisa kamu coba !</center></h1>
+                            <h1>
+                                <center>
+                                    Daftar Soal yang bisa kamu coba !
+                                </center>
+                            </h1>
                             <Row className="show-grid d-flex justify-content-center">
-                                {
-                                    (this.state.dataSoal).length > 0 ? this.state.dataSoal.map(function(data) {
-                                        return(<CardSoal 
-                                            img={data.image}
-                                            title={data.titleSoal}
-                                            description={data.descriptionSoal}
-                                            id={data.id}
-                                        />)
-                                    }) : <div><center><Spinner animation="border" variant="primary" /></center></div>
-                                }
                             </Row>
                         </Col>
                     </Row>
